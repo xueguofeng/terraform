d7623fe8dcbc86b1dba9da4f4cbb596bfdb6955a
xueguofeng <42553657+xueguofeng@users.noreply.github.com>
2022-02-06 10:45:37 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "remote" {
+    organization = "example-org-1d8e2c"
+    workspaces {
+      name = "integration"
+    }
+  }
+  required_version = ">= 0.13.0"
+}
+
+
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    # Ubuntu 20:    "ami-0892d3c7ee96c0bf7"
+    # Amazon Linux: "ami-066333d9c572b0680"
+
+    us-west-2 = "ami-0892d3c7ee96c0bf7"
+    us-east-1 = "ami-04505e74c0741db8d"
+  }
+}
+
+resource "aws_instance" "rx-terraform-vm" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  key_name      = "richardx"
+  tags = {
+    Name = "rx-terraform-vm"
+  }
+}
+
+output "my-output" {
+  value = aws_instance.rx-terraform-vm
+}
